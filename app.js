cdba9b5cb46bb87e54c632ab74183cc6f0dfc7a1
stormsoundar <soundarit1995@gmail.com>
2022-06-25 21:05:37 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import morgan from 'morgan';
+import connectDB from './config/db/db.js';
+import { NODE_ENV, PORT } from './loaders/config.js';
+import mainRouter from './routes/index.js';
+import errorHandler from './middleware/error.js';
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Connect DB
+connectDB();
+
+// Logging middleware
+app.use(morgan('dev'));
+
+// Mount Routes
+app.use('/', mainRouter);
+
+// Error Handler
+app.use(errorHandler);
+
+app.get('/', (req, res, next) => {
+  res.send({ message: 'Api connected!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running in ${NODE_ENV} mode on port ${PORT}`);
+});
